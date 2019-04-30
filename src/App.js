441c2d6c615092d636e061b27276ccc77db1c5a3
REDDY PRASAD <dev.drprasad@aim.com>
2019-04-30 17:22:57 +0530

    :tada::tada: Initial Commit :tada::tada:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AceEditor from "react-ace";
+import domtoimage from "dom-to-image";
+
+import "brace/mode/javascript";
+import "brace/theme/dracula";
+
+import "./App.css";
 
 function App() {
+  const ref = React.useRef(null);
+
+  const download = () => {
+    domtoimage.toBlob(ref.current).then(blob => {
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(blob);
+      a.download = `card-${Date.now() + Math.floor(Math.random() * 5000)}`;
+      a.click();
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="CardWrapper-CropBug">
+        <div className="Card" ref={ref}>
+          <div className="CardInner">
+            <div className="Content" contentEditable />
+            <AceEditor
+              className="CodeEditor"
+              mode="javascript"
+              theme="dracula"
+              name="code-editor"
+              width="100%"
+              height="100%"
+              fontSize={20}
+              showGutter={false}
+              tabSize={2}
+              highlightActiveLine={false}
+              highlightGutterLine={false}
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <button type="button" onClick={download}>
+          Download
+        </button>
+      </div>
     </div>
   );
 }
